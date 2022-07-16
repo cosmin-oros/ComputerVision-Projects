3cc6b09fd429b9d20bd994dab830d270680a6b5b
cosmin-oros <oroscosmin09@gmail.com>
2022-07-16 11:40:58 +0300

    update

--- ColorRecognition/main.py
+++ ColorRecognition/main.py
@@ -2,10 +2,48 @@ import cv2
 
 # 0 -> first webcam
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
     # get a frame
     _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    height, width, _ = frame.shape
+
+    cx = int(width / 2)
+    cy = int(height / 2)
+
+    pixel_center = hsv_frame[cy, cx]
+    hue_value = pixel_center[0]
+
+    color = "Undefined"
+    if hue_value < 5:
+        color = "Red"
+    elif hue_value < 22:
+        color = "Orange"
+    elif hue_value < 33:
+        color = "Yellow"
+    elif hue_value < 78:
+        color = "Green"
+    elif hue_value < 131:
+        color = "Blue"
+    elif hue_value < 167:
+        color = "Purple"
+    else:
+        color = "Red"
+
+    print(pixel_center)
+    pixel_center_bgr = frame[cy, cx]
+    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+    cv2.putText(frame , color, (10 , 50), 0, 1.5, (b, g, r), 2)
+    cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
 
     cv2.imshow("Frame", frame)
-    cv2.waitKey(0)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
