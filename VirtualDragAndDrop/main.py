c00810d8316e59ae0372ba9e8211b0ae069bd1f7
cosmin-oros <oroscosmin09@gmail.com>
2022-07-19 16:50:15 +0300

    virtual drag and drop update2

--- VirtualDragAndDrop/main.py
+++ VirtualDragAndDrop/main.py
@@ -9,11 +9,35 @@ cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
 
+colorR = (255, 0, 255)
+
+cx, cy, w, h = 100, 100, 200, 200
+
+class DragRect():
+    def __init__(self, posCenter, size=[200, 200]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def update(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            colorR = 0, 255, 0
+            cx, cy = cursor
+
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     img = detector.findHands(img)
     lmList, _ = detector.findPosition(img)
+
+    if lmList:
+        l, _, _ = detector.findDistance(8, 12, img)
+        if l < 30:
+            cursor = lmList[8]
+
+    cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-
