e617a726858f9194d98aa51c26a11088e97defa6
cosmin-oros <oroscosmin09@gmail.com>
2022-07-19 16:56:43 +0300

    virtual drag and drop update3

--- VirtualDragAndDrop/main.py
+++ VirtualDragAndDrop/main.py
@@ -13,6 +13,7 @@ colorR = (255, 0, 255)
 
 cx, cy, w, h = 100, 100, 200, 200
 
+
 class DragRect():
     def __init__(self, posCenter, size=[200, 200]):
         self.posCenter = posCenter
@@ -23,8 +24,12 @@ class DragRect():
         w, h = self.size
 
         if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
-            colorR = 0, 255, 0
-            cx, cy = cursor
+            self.posCenter = cursor
+
+
+rectList = []
+for x in range(5):
+    rectList.append(DragRect([x*250+150, 150]))
 
 while True:
     success, img = cap.read()
@@ -33,9 +38,15 @@ while True:
     lmList, _ = detector.findPosition(img)
 
     if lmList:
-        l, _, _ = detector.findDistance(8, 12, img)
+        l, _, _ = detector.findDistance(8, 12, img, draw=False)
         if l < 30:
             cursor = lmList[8]
+            for rect in rectList:
+                rect.update(cursor)
+
+    for rect in rectList:
+        cx, cy = rect.posCenter
+        w, h = rect.size
 
     cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
 
