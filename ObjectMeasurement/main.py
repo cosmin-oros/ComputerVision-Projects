68e158607625daa5c261531b2ad359bb1af9fcb0
cosmin-oros <oroscosmin09@gmail.com>
2022-07-20 19:23:16 +0300

    object measurement update1

---
+++ ObjectMeasurement/main.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+webcam = True
+# path = '' for photos
+path = ''
+cap = cv2.VideoCapture(0)
+cap.set(10, 160)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+while True:
+    if webcam:
+        success, img = cap.read()
+    else:
+        img = cv2.imread(path)
+        img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
+
+    cv2.imshow('Original', img)
+    cv2.waitKey(1)
