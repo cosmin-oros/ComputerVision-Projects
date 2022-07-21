f303f603f5bbfd7673d0ef2bf5de6b5da2a899b3
cosmin-oros <oroscosmin09@gmail.com>
2022-07-21 17:03:22 +0300

    virtual keyboard update

--- AIVirtualKeyboard/main.py
+++ AIVirtualKeyboard/main.py
@@ -1,3 +1,19 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8)
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
+
+    cv2.rectangle(img, (100, 100), (200, 200), (255, 0, 255), cv2.FILLED)
+    cv2.putText(img, "Q", (140, 140), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
