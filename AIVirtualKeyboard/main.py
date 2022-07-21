83989f18f24b328f02699f1bea55d47883895300
cosmin-oros <oroscosmin09@gmail.com>
2022-07-21 19:24:34 +0300

    virtual keyboard update2

--- AIVirtualKeyboard/main.py
+++ AIVirtualKeyboard/main.py
@@ -7,13 +7,30 @@ cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
 
+
+class Button():
+    def __init__(self, pos, text, size=[100, 100]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+
+    def draw(self, img):
+        x, y = self.pos
+        w, h =self.size
+
+        cv2.rectangle(img, self.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, self.text, (x+25, y+25), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+        return img
+
+
+myButton = Button([100, 100], "Q")
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bboxInfo = detector.findPosition(img)
 
-    cv2.rectangle(img, (100, 100), (200, 200), (255, 0, 255), cv2.FILLED)
-    cv2.putText(img, "Q", (140, 140), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    img = myButton.draw(img)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
