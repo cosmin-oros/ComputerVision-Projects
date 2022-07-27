180c1dd3ca821d1dd75b9d41280bcffd1a9c26d8
cosmin-oros <oroscosmin09@gmail.com>
2022-07-27 16:58:09 +0300

    parking space video capture

---
+++ ParkingSpaceCounter/main.py
@@ -0,0 +1,12 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+# Video feed
+cap = cv2.VideoCapture('carPark.mp4')
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+width, height = 107, 48
